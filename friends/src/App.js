e693bcd5c6f0cd5b2c56961a4b67518c1381c7fb
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-20 14:46:09 -0500

    Setting up App for Login form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./components/login/Login.js";
 
 function App() {
   return (
     <div className="App">
-      It's working
+      <Login />
     </div>
   );
 }
